7269524a605408f71381d4c2e9495bcf41d00c75
Louis Bataillard <info@mobweb.ch>
2014-05-29 12:47:30 +0200

    initial commit

---
+++ app/code/local/MobWeb/CustomBodyClasses/Block/Html.php
@@ -0,0 +1,21 @@
+<?php
+
+class MobWeb_CustomBodyClasses_Block_Html extends Mage_Page_Block_Html
+{
+    // Modified this function to allow an array as $className, where all elements
+    // are added as individual classes
+    public function addBodyClass($className)
+    {
+        if(!is_array($className)) {
+            $classNames = array($className);
+        } else {
+            $classNames = $className;
+        }
+
+        foreach($classNames AS $className) {
+            $className = preg_replace('#[^a-z0-9]+#', '-', strtolower($className));
+            $this->setBodyClass($this->getBodyClass() . ' ' . $className);
+        }
+        return $this;
+    }
+}
---
+++ app/code/local/MobWeb/CustomBodyClasses/Helper/Data.php
@@ -0,0 +1,19 @@
+<?php
+
+class MobWeb_CustomBodyClasses_Helper_Data extends Mage_Core_Helper_Abstract
+{
+	public function getCustomBodyClasses()
+	{
+		// Collect custom body classes
+		$customBodyClasses = array();
+
+		// Example: Check if the current user is logged in
+		$customBodyClasses[] = Mage::getSingleton('customer/session')->isLoggedIn() ? 'logged-in' : 'not-logged-in';
+
+		// Do whatever you want here...
+		$customBodyClasses[] = 'another-class';
+
+		// Return all body classes here
+		return $customBodyClasses;
+	}
+}
